f5e4481fd43a0d25ab4181156b3f91a98709b256
HenaryKhuraijam <henarykhu123@gmial.com>
2023-09-07 16:54:34 +0530

    Add postLike function

--- src/modules/api_manager.js
+++ src/modules/api_manager.js
@@ -2,6 +2,8 @@ const reqBaseURL = 'https://thronesapi.com/api/v2/Characters/';
 const BaseURL = new Request(reqBaseURL);
 
 const InvolvementURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+const appBaseURL = `${InvolvementURL}KfFQBfBx4KVrKL4cLb5E/`;
+const likeURL = new Request(`${appBaseURL}likes/`);
 
 const getCode = async () => {
   await fetch(InvolvementURL, {
@@ -13,9 +15,23 @@ const getCode = async () => {
     .then((response) => console.log(response));
 };
 
+const postLike = async (obj) => {
+  fetch(likeURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(obj),
+  })
+    .then((response) => response.text())
+    .then((likeData) => {
+      console.log(likeData);
+    });
+};
+
 const getChars = async (callback) => {
   const response = await fetch(BaseURL).then((response) => response.json());
   callback(response);
 };
 
-export { getChars, getCode };
+export { getChars, getCode, postLike };
